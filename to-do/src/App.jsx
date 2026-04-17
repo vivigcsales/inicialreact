2a0d09edf238b606db2d40db0b4dfffb3c2ac19b
vivi 3 <vitoriagcavalcantesales@gmail.com>
2026-04-17 00:40:45 -0300

    add new functionalities, form and components

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,30 +1,56 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+
+import './App.css';
 
 function App() {
-const [todos, setTodos] = useState([
-  {
-    id: 1,
-    text: "Criar funcionalidade x no sistema",
-    category: "Trabalho",
-    isCompleted: false
-  },
-  {
-    id: 2,
-    text:"Ir para a academia",
-    category: "Pessoal",
-    isCompleted: false
-  },
-  {
-    id: 3,
-    text:"Estudar React",
-    category: "Estudos",
-    isCompleted: false
-  },
-])
-  return <div className="app">
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
 
-  </div>
+  const addTodo = (text, category) => {
+    const newTodos = [...todos,
+      {
+        id: Math.floor(Math.random() * 1000),
+        text,
+        category,
+        isCompleted: false,
+      }
+    ];
+
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
-export default App
+export default App;
